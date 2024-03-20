ac4cfdfa2c84b3f4b6d789bc4a67faa7c0c39f3c
Abin <abinvs2019@gmail.com>
2024-03-20 22:02:42 +0530

    [Firebase-auth] updated signup trigger

--- js/signup.js
+++ js/signup.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 
 
 const signupForm = document.getElementById('signup-form');
@@ -44,7 +44,7 @@ const signupForm = document.getElementById('signup-form');
       }
 
       // Create user with email and password
-      firebase.auth().createUserWithEmailAndPassword(email, password)
+      createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
           // User signed up successfully
           const user = userCredential.user;
